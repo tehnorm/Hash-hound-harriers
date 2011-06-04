d970b51ca52b6075ac525bce9b3202337e3302ae
Carl Licata <carl@carl.is>
2011-06-04 11:15:28 -0400

    Fixing Game API skeleton

--- server/resources/game.php
+++ server/resources/game.php
@@ -1,65 +1,74 @@
 <?php
 /**
- * Creates a Game
+ * Game
  *
- * POST /game
- *  output: HTTP OK + game (if successful),
- *					HTTP BADREQUEST (if incorrect params),
- *					HTTP INTERNALSERVERERROR (if unforeseen error)
- *
- * @uri /game
+ * @uri /game(/.*)?
  */
 class GameResource extends Resource {
-	function post($request) {
+	/**
+	 * Routes GET requests to the appropriate API function
+	 */
+	function get($request) {
+		
 	}
-}
 
-/**
- * Gets a Game
- *
- * GET /game/{id}
- *  output: HTTP OK + game (if successful),
- *					HTTP NOTFOUND (if not game exists for that id),
- *					HTTP INTERNALSERVERERROR (if unforeseen error)
- *
- * @uri /game/(.*)+
- */
-class GameResource extends Resource {
-	function get($request, $id) {
-	}
-}
-
-/**
- * Adds a point to the game
- *
- * POST /game/point
- *   input: game_id, type, lat, long, direction
- *  output: HTTP OK (if successful),
- *					HTTP BADREQUEST (if incorrect params),
- *					HTTP NOTFOUND (if no game exists for that id),
- *					HTTP INTERNALSERVERERROR (if unforeseen error)
- *
- * @uri /game/point
- */
-class GameResource extends Resource {
+	/**
+	 * Routes POST requests to the appropriate API function
+	 */
 	function post($request) {
-	}
-}
 
-/**
- * Adds a user to the game
- *
- * POST /game/user
- *   input: game_id, user_id
- *  output: HTTP OK (if successful),
- *					HTTP BADREQUEST (if incorrect params),
- *					HTTP NOTFOUND (if no game exists for that id; if no user exists for that id),
- *					HTTP INTERNALSERVERERROR (if unforeseen error)
- *
- * @uri /game/user
- */
-class GameResource extends Resource {
-	function post($request) {
+	}
+
+
+	/**
+	 * Creates a Game
+	 *
+	 * POST /game
+	 *  output: HTTP OK + game (if successful),
+	 *					HTTP BADREQUEST (if incorrect params),
+	 *					HTTP INTERNALSERVERERROR (if unforeseen error)
+	 */
+	function create_game($request) {
+		
+	}
+	/**
+	 * Gets a Game
+	 *
+	 * GET /game/{id}
+	 *  output: HTTP OK + game (if successful),
+	 *					HTTP NOTFOUND (if not game exists for that id),
+	 *					HTTP INTERNALSERVERERROR (if unforeseen error)
+	 */
+	function get_game($request) {
+		
+	}
+
+	/**
+	 * Adds a point to the game
+	 *
+	 * POST /game/point
+	 *   input: game_id, type, lat, long, direction
+	 *  output: HTTP OK (if successful),
+	 *					HTTP BADREQUEST (if incorrect params),
+	 *					HTTP NOTFOUND (if no game exists for that id),
+	 *					HTTP INTERNALSERVERERROR (if unforeseen error)
+	 */
+	function add_point($request) {
+		
+	}
+
+	/**
+	 * Adds a user to the game
+	 *
+	 * POST /game/user
+	 *   input: game_id, user_id
+	 *  output: HTTP OK (if successful),
+	 *					HTTP BADREQUEST (if incorrect params),
+	 *					HTTP NOTFOUND (if no game exists for that id; if no user exists for that id),
+	 *					HTTP INTERNALSERVERERROR (if unforeseen error)
+	 */
+	function add_user($request) {
+		
 	}
 }
 ?>
