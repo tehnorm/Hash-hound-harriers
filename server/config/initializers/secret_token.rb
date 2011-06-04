c3317d3f86cd034855d69dc8fe3f446c62eda27e
carlyle <carl@carl.is>
2011-06-04 00:15:16 -0400

    Removing Rails (we are switching to a PHP framework)

--- server/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Server::Application.config.secret_token = 'eed562deacd564407f4a76f0af4226d017d2a8e7f8b5f7bd441cfee6a3bf3016190da494ea61f06cd4580a77739fbe9ec429d7ed2cefdad06b3bda61430d5aa3'
