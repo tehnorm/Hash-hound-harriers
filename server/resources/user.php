4e23385af2ccf1c8876d36a50d60335e4dd2c7d3
carlyle <carl@carl.is>
2011-06-04 10:10:24 -0400

    Creating skeleton for User API

--- server/resources/user.php
+++ server/resources/user.php
@@ -1,16 +1,30 @@
 <?php
 /**
- * The User Resource
+ * Creates a user
+ *
+ * POST /user
+ *   input: device_id, lat, long, name, email
+ *  output: user_id (if successful) HTTP 411 LENGTHREQUIRED (if no params)
+ *
  * @uri /user
  */
 
 class UserResource extends Resource {
-	function get($request) {
-		$response = new Response($request);
-		$response->code = Response::OK;
-		$response->addHeader('content-type', 'text/plain');
-		$response->body = "This is a user.";
-		return $response;
+	function post($request) {
+	}
+}
+
+/**
+ * Allows the user to check their location against available points (also updates the user's location)
+ *
+ * POST /user/check_location
+ *   input: game_id, user_id, lat, long
+ *  output: point
+ *
+ * @uri /user/check_location
+ */
+class UserResource extends Resource {
+	function post($request) {
 	}
 }
 ?>
