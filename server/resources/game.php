c772a6dfd7a8634308b6635088ede35573b99b0a
Todd Mosier <toddmosier@gmail.com>
2011-06-04 20:34:17 -0400

    adding get_game

--- server/resources/game.php
+++ server/resources/game.php
@@ -12,8 +12,8 @@ class GameResource extends Resource {
 		$response = new Response($request);
 
 		if (preg_match("/\/game(\/(?P<id>.*))?/", $request->uri, $matches)) {
-			if (is_numeric($matches["id"])) {
-				$id = intval($matches["id"]);
+			if (is_string($matches["id"])) {
+				$id = $matches["id"];
 				$response = $this->get_game($request, $id);
 			} else {
 				$response->code = Response::BADREQUEST;
@@ -118,10 +118,21 @@ class GameResource extends Resource {
 	function get_game($request, $id) {
 		$response = new Response($request);
 		
-		try {
-			$response->code = Response::OK;
-			$response->addHeader("Content-Type", "text/plain");
-			$response->body = "Getting Game #$id";
+		$bad_request_response = new Response($request);
+		$bad_request_response->code = Response::BADREQUEST;
+		$bad_request_response->addHeader("Content-Type", "text/plain");
+		$bad_request_response->body = "Expected id";
+
+    try{
+      $mongo = new Mongo(DB_SERVER);
+      $db = $mongo->hhh;
+      $game_collection = $db->games;
+      $mongo_game_id = new MongoId($id);
+      $game = $game_collection->findOne(array("_id" => $mongo_game_id));
+
+      $response->code = Response::OK;
+      $response->addHeader("Content-Type", "application/json");
+      $response->body = json_encode($game);
 		} catch (Exception $e) {
 			$response->code = Response::INTERNALSERVERERROR;
 			$response->addHeader("Content-Type", "text/plain");
